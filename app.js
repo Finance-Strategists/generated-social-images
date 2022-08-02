b97872b19255715675c202e579b51d6fbf2ba391
Algen <adelacruz@innovuze.com>
2022-08-02 19:01:43 +0800

    Working Static Maps API request

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
+const config = require('./config.json');
 
-console.log('Password', process.env.PASSWORD);
+const API_Key = config.API_KEY;
 
-setTimeout(() => {
+global.changeMap = function () {
 
   const location = document.getElementById('location').innerHTML
-  
+
   console.log(location);
 
-  document.getElementById("static-map").src = "https://maps.googleapis.com/maps/api/staticmap?center=" + location + "&zoom=14&size=1080x1080&key=YOUR_API_KEY";
-}, 5000);
+  document.getElementById("static-map").src = "https://maps.googleapis.com/maps/api/staticmap?center=" + location + "&zoom=10&size=1080x1080&key=" + API_Key;
+
+};
