e873c83cceba7d720242f82f7b25807a66135768
Algen <adelacruz@innovuze.com>
2022-08-07 09:05:59 +0800

    Debugging unknown hashed images emit

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,52 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   devServer: {
-    open: true
+    open: true,
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+              options: {
+              esModule: false,
+            },
+          }
+        ]
       },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              esModule: false,
+            },
+          },
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg|jpg|ico)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'assets',
+              name: "[name].[ext]",
+              outputPath: "assets",
               emitFile: true,
               esModule: false,
-            }
+            },
           },
         ],
       },
@@ -40,7 +55,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-       template: './src/index.html'
-    })
-  ]
+      template: "./src/index.html",
+    }),
+  ],
 };
